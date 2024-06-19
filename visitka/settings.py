93e1b5663a1d94251f1a145bdd6b67e768978fd8
Misha Naradka <104830399+Kirito85@users.noreply.github.com>
2024-06-19 22:25:25 +0300

    settings

--- visitka/settings.py
+++ visitka/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bprkvn9ht-8cq#6e*xdo83j%kz&%++188u225cre@4l)f476tb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["vercel"]
 
 
 # Application definition

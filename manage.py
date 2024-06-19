7858b56ebe660543754942eed5da2a7207d31be4
Misha Naradka <104830399+Kirito85@users.noreply.github.com>
2024-06-20 00:42:23 +0300

    wsgi

--- manage.py
+++ manage.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 import os
 import sys
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
-app = get_wsgi_application()
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'visitka.settings')
+
+application = Cling(get_wsgi_application())
 
 
 def main():
